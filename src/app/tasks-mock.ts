dbf0f8ce074f321d0350a002b880ae1734047ff6
Alexandr <valverin95@gmail.com>
2020-04-15 20:11:44 +0300

    feat: tasksMock - added new item

--- src/app/tasks-mock.ts
+++ src/app/tasks-mock.ts
@@ -1,5 +1,7 @@
 import { Task } from './task';
 
 export const TASKS: Task[] = [
-    { id: 1, done: false, title: 'Example Task', description: 'This is description for task item'}
+    { id: 1, done: false, title: 'Example Task', description: 'This is description for task item'},
+    { id: 2, done: false, title: 'Example Task 2', description: 'This is description for task item'},
+    { id: 3, done: false, title: 'Example Task 3', description: 'This is description for task item'},
 ]
