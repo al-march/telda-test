d2b6b2d6e8db138235036acdf2f5498eb770c6bb
Alexandr <valverin95@gmail.com>
2020-04-15 16:43:43 +0300

    feat: added taskItem

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,10 +5,11 @@ import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
 import { TasksNewComponent } from './tasks-new/tasks-new.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksItemComponent } from './tasks-list/tasks-item/tasks-item.component';
 
 
 @NgModule({
-  declarations: [TasksComponent, TasksNewComponent, TasksListComponent],
+  declarations: [TasksComponent, TasksNewComponent, TasksListComponent, TasksItemComponent],
   imports: [
     CommonModule,
     TasksRoutingModule
